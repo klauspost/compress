fa08a787327fffa9a47ddc53437f3b10fb921344
Klaus Post <klauspost@gmail.com>
2015-10-31 12:37:28 +0100

    Add zlib inflate tests.

--- flate/inflate_test.go
+++ flate/inflate_test.go
@@ -7,6 +7,9 @@ package flate
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -36,4 +39,135 @@ func TestReset(t *testing.T) {
 			t.Errorf("inflated[%d]:\ngot  %q\nwant %q", i, inflated[i], s)
 		}
 	}
+}
+
+// Tests ported from zlib/test/infcover.c
+type infTest struct {
+	hex string
+	id  string
+	n   int
+}
+
+var infTests = []infTest{
+	infTest{"0 0 0 0 0", "invalid stored block lengths", 1},
+	infTest{"3 0", "fixed", 0},
+	infTest{"6", "invalid block type", 1},
+	infTest{"1 1 0 fe ff 0", "stored", 0},
+	infTest{"fc 0 0", "too many length or distance symbols", 1},
+	infTest{"4 0 fe ff", "invalid code lengths set", 1},
+	infTest{"4 0 24 49 0", "invalid bit length repeat", 1},
+	infTest{"4 0 24 e9 ff ff", "invalid bit length repeat", 1},
+	infTest{"4 0 24 e9 ff 6d", "invalid code -- missing end-of-block", 1},
+	infTest{"4 80 49 92 24 49 92 24 71 ff ff 93 11 0", "invalid literal/lengths set", 1},
+	infTest{"4 80 49 92 24 49 92 24 f b4 ff ff c3 84", "invalid distances set", 1},
+	infTest{"4 c0 81 8 0 0 0 0 20 7f eb b 0 0", "invalid literal/length code", 1},
+	infTest{"2 7e ff ff", "invalid distance code", 1},
+	infTest{"c c0 81 0 0 0 0 0 90 ff 6b 4 0", "invalid distance too far back", 1},
+
+	// also trailer mismatch just in inflate()
+	infTest{"1f 8b 8 0 0 0 0 0 0 0 3 0 0 0 0 1", "incorrect data check", -1},
+	infTest{"1f 8b 8 0 0 0 0 0 0 0 3 0 0 0 0 0 0 0 0 1", "incorrect length check", -1},
+	infTest{"5 c0 21 d 0 0 0 80 b0 fe 6d 2f 91 6c", "pull 17", 0},
+	infTest{"5 e0 81 91 24 cb b2 2c 49 e2 f 2e 8b 9a 47 56 9f fb fe ec d2 ff 1f", "long code", 0},
+	infTest{"ed c0 1 1 0 0 0 40 20 ff 57 1b 42 2c 4f", "length extra", 0},
+	infTest{"ed cf c1 b1 2c 47 10 c4 30 fa 6f 35 1d 1 82 59 3d fb be 2e 2a fc f c", "long distance and extra", 0},
+	infTest{"ed c0 81 0 0 0 0 80 a0 fd a9 17 a9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 6", "window end", 0},
+}
+
+func TestInflate(t *testing.T) {
+	for _, test := range infTests {
+		hex := strings.Split(test.hex, " ")
+		data := make([]byte, len(hex))
+		for i, h := range hex {
+			b, _ := strconv.ParseInt(h, 16, 32)
+			data[i] = byte(b)
+		}
+		buf := bytes.NewReader(data)
+		r := NewReader(buf)
+
+		_, err := io.Copy(ioutil.Discard, r)
+		if (test.n == 0 && err == nil) || (test.n != 0 && err != nil) {
+			t.Logf("%q: OK:", test.id)
+			t.Logf(" - got %v", err)
+			continue
+		}
+
+		if test.n == 0 && err != nil {
+			t.Errorf("%q: Expected no error, but got %v", test.id, err)
+			continue
+		}
+
+		if test.n != 0 && err == nil {
+			t.Errorf("%q:Expected an error, but got none", test.id, err)
+			continue
+		}
+		t.Fatal(test.n, err)
+	}
+
+	for _, test := range infOutTests {
+		hex := strings.Split(test.hex, " ")
+		data := make([]byte, len(hex))
+		for i, h := range hex {
+			b, _ := strconv.ParseInt(h, 16, 32)
+			data[i] = byte(b)
+		}
+		buf := bytes.NewReader(data)
+		r := NewReader(buf)
+
+		_, err := io.Copy(ioutil.Discard, r)
+		if test.err == (err != nil) {
+			t.Logf("%q: OK:", test.id)
+			t.Logf(" - got %v", err)
+			continue
+		}
+
+		if test.err == false && err != nil {
+			t.Errorf("%q: Expected no error, but got %v", test.id, err)
+			continue
+		}
+
+		if test.err && err == nil {
+			t.Errorf("%q: Expected an error, but got none", test.id)
+			continue
+		}
+		t.Fatal(test.err, err)
+	}
+
+}
+
+// Tests ported from zlib/test/infcover.c
+// Since zlib inflate is push (writer) instead of pull (reader)
+// some of the window size tests have been removed, since they
+// are irrelevant.
+type infOutTest struct {
+	hex    string
+	id     string
+	step   int
+	win    int
+	length int
+	err    bool
+}
+
+var infOutTests = []infOutTest{
+	infOutTest{"2 8 20 80 0 3 0", "inflate_fast TYPE return", 0, -15, 258, false},
+	infOutTest{"63 18 5 40 c 0", "window wrap", 3, -8, 300, false},
+	infOutTest{"e5 e0 81 ad 6d cb b2 2c c9 01 1e 59 63 ae 7d ee fb 4d fd b5 35 41 68 ff 7f 0f 0 0 0", "fast length extra bits", 0, -8, 258, true},
+	infOutTest{"25 fd 81 b5 6d 59 b6 6a 49 ea af 35 6 34 eb 8c b9 f6 b9 1e ef 67 49 50 fe ff ff 3f 0 0", "fast distance extra bits", 0, -8, 258, true},
+	infOutTest{"3 7e 0 0 0 0 0", "fast invalid distance code", 0, -8, 258, true},
+	infOutTest{"1b 7 0 0 0 0 0", "fast invalid literal/length code", 0, -8, 258, true},
+	infOutTest{"d c7 1 ae eb 38 c 4 41 a0 87 72 de df fb 1f b8 36 b1 38 5d ff ff 0", "fast 2nd level codes and too far back", 0, -8, 258, true},
+	infOutTest{"63 18 5 8c 10 8 0 0 0 0", "very common case", 0, -8, 259, false},
+	infOutTest{"63 60 60 18 c9 0 8 18 18 18 26 c0 28 0 29 0 0 0", "contiguous and wrap around window", 6, -8, 259, false},
+	infOutTest{"63 0 3 0 0 0 0 0", "copy direct from output", 0, -8, 259, false},
+	infOutTest{"1f 8b 0 0", "bad gzip method", 0, 31, 0, true},
+	infOutTest{"1f 8b 8 80", "bad gzip flags", 0, 31, 0, true},
+	infOutTest{"77 85", "bad zlib method", 0, 15, 0, true},
+	infOutTest{"78 9c", "bad zlib window size", 0, 8, 0, true},
+	infOutTest{"1f 8b 8 1e 0 0 0 0 0 0 1 0 0 0 0 0 0", "bad header crc", 0, 47, 1, true},
+	infOutTest{"1f 8b 8 2 0 0 0 0 0 0 1d 26 3 0 0 0 0 0 0 0 0 0", "check gzip length", 0, 47, 0, true},
+	infOutTest{"78 90", "bad zlib header check", 0, 47, 0, true},
+	infOutTest{"8 b8 0 0 0 1", "need dictionary", 0, 8, 0, true},
+	infOutTest{"63 18 68 30 d0 0 0", "force split window update", 4, -8, 259, false},
+	infOutTest{"3 0", "use fixed blocks", 0, -15, 1, false},
+	infOutTest{"", "bad window size", 0, 1, 0, true},
 }
