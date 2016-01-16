ef0c3a2756447dc1dc3449407c8df0fd258f4d8e
Klaus Post <klauspost@gmail.com>
2016-01-16 10:33:55 +0100

    Simpler miss counting.

--- flate/snappy.go
+++ flate/snappy.go
@@ -42,13 +42,14 @@ func snappyEncode(dst *tokens, src []byte) {
 		tableSize *= 2
 	}
 	var table [maxTableSize]int
-	var misses int
+
 	// Iterate over the source bytes.
 	var (
 		s   int // The iterator position.
 		t   int // The last position with the same hash as s.
 		lit int // The start position of any pending literal bytes.
 	)
+
 	for s+3 < len(src) {
 		// Update the hash table.
 		b0, b1, b2, b3 := src[s], src[s+1], src[s+2], src[s+3]
@@ -61,9 +62,8 @@ func snappyEncode(dst *tokens, src []byte) {
 		t, *p = *p-1, s+1
 		// If t is invalid or src[s:s+4] differs from src[t:t+4], accumulate a literal byte.
 		if t < 0 || s-t >= maxOffset || b0 != src[t] || b1 != src[t+1] || b2 != src[t+2] || b3 != src[t+3] {
-			misses++
 			// Skip 1 byte for 16 consecutive missed.
-			s += 1 + (misses >> 4)
+			s += 1 + ((s - lit) >> 4)
 			continue
 		}
 		// Otherwise, we have a match. First, emit any pending literal bytes.
@@ -81,7 +81,6 @@ func snappyEncode(dst *tokens, src []byte) {
 			s++
 			t++
 		}
-		misses = 0
 		// Emit the copied bytes.
 		// inlined: emitCopy(dst, s-t, s-s0)
 
