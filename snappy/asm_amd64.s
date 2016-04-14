8b650e22602fb55e69119481cd626dc1d78c8c02
Nigel Tao <nigeltao@golang.org>
2016-04-15 09:36:18 +1000

    Remove DUPOK and NOPROF flags from asm functions.
    runtime/textflag.h defines:
    NOPROF  1
    DUPOK   2
    NOSPLIT 4
    
    The DUPOK bit was why the bug in
    https://github.com/klauspost/compress/commit/6ea72a38a8cdbe25966059c547d425e580c624a7
    wasn't picked up earlier.
    
    The existing 7 is presumably a copy/paste of some standard library asm
    code. It's a legacy idiom that really should be 5 or 4. See
    https://groups.google.com/d/msg/golang-nuts/uJTYuruvnZM/6j4KXgkdcf0J for
    some history.
    
    Asm code in the standard library uses NOSPLIT instead of 4 these days.

--- snappy/asm_amd64.s
+++ snappy/asm_amd64.s
@@ -3,8 +3,10 @@
 
 // Copyright 2015, Klaus Post, see LICENSE for details.
 
+#include "textflag.h"
+
 // func matchLenSSE4(a, b []byte, max int) int
-TEXT ·matchLenSSE4(SB), 7, $0
+TEXT ·matchLenSSE4(SB), NOSPLIT, $0
 	MOVQ  a+0(FP), SI        // RSI: &a
 	MOVQ  b+24(FP), DI       // RDI: &b
 	MOVQ  max+48(FP), R10    // R10: max
