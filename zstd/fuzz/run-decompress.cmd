f52159f2330225faa9a0c03f472c44ce781894f1
Klaus Post <klauspost@gmail.com>
2019-04-23 15:34:48 +0200

    Move fuzz data into testdata. Should be more gomod friendly.

--- zstd/fuzz/run-decompress.cmd
+++
@@ -1,7 +0,0 @@
-REM go get -u github.com/dvyukov/go-fuzz/...
-cd ..
-go-fuzz-build -tags=decompress github.com/klauspost/compress/zstd
-cd fuzz
-del /Q decompress\crashers\*.*
-del /Q decompress\suppressions\*.*
-go-fuzz -bin=../zstd-fuzz.zip -workdir=decompress -procs=4
