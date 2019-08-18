91a346311aa4e591b307e83992614145d5a7de76
Klaus Post <klauspost@gmail.com>
2019-08-18 19:17:49 +0200

    Add fuzzit CI

---
+++ fuzzit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xe
+
+## Build fuzzing targets
+## go-fuzz doesn't support modules for now, so ensure we do everything
+## in the old style GOPATH way
+export GO111MODULE="off"
+
+## Install go-fuzz
+go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
+
+go-fuzz-build -libfuzzer -o flate.a github.com/klauspost/compress-fuzz/flate
+clang -fsanitize=fuzzer flate.a -o flate
+
+## Install fuzzit specific version for production or latest version for development :
+# https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
+wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.23/fuzzit_Linux_x86_64
+chmod a+x fuzzit
+
+## upload fuzz target for long fuzz testing on fuzzit.dev server or run locally for regression
+./fuzzit create job --type ${1} klauspost/compress-flate flate
