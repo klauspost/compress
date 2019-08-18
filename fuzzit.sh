ec8f403c0cbc24d399fc03feaab9783593b4ad6d
Klaus Post <klauspost@gmail.com>
2019-08-18 20:20:21 +0200

    rename binary to flate-fuzz

--- fuzzit.sh
+++ fuzzit.sh
@@ -10,7 +10,7 @@ export GO111MODULE="off"
 go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 
 go-fuzz-build -libfuzzer -o flate.a github.com/klauspost/compress-fuzz/flate
-clang -fsanitize=fuzzer flate.a -o flate
+clang -fsanitize=fuzzer flate.a -o flate-fuzz
 
 ## Install fuzzit specific version for production or latest version for development :
 # https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
@@ -18,4 +18,4 @@ wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.23/
 chmod a+x fuzzit
 
 ## upload fuzz target for long fuzz testing on fuzzit.dev server or run locally for regression
-./fuzzit create job --type ${1} klauspost/compress-flate flate
+./fuzzit create job --type ${1} klauspost/compress-flate flate-fuzz
