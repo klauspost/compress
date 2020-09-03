28b892527237039874c2c0a1eeb4e052975c12a8
Klaus Post <klauspost@gmail.com>
2020-09-03 03:24:41 -0700

    zstd: Add experimental encoder dictionaries (#281)
    Add encoder dictionary support.
    
    Currently uses the dictionary content and literal compression table.
    
    The only unused elements are the FSE tables.
    
    ```
    BenchmarkEncodeAllDict/length-19-level-fastest-dict-1
    BenchmarkEncodeAllDict/length-19-level-fastest-dict-1-32         	  141180	      8351 ns/op	   2.28 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-19-level-default-dict-1
    BenchmarkEncodeAllDict/length-19-level-default-dict-1-32         	   19639	     60085 ns/op	   0.32 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-19-level-better-dict-1
    BenchmarkEncodeAllDict/length-19-level-better-dict-1-32          	    5714	    196535 ns/op	   0.10 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-12131-level-fastest-dict-1
    BenchmarkEncodeAllDict/length-12131-level-fastest-dict-1-32      	   19077	     59129 ns/op	 205.16 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-12131-level-default-dict-1
    BenchmarkEncodeAllDict/length-12131-level-default-dict-1-32      	    3999	    301825 ns/op	  40.19 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-12131-level-better-dict-1
    BenchmarkEncodeAllDict/length-12131-level-better-dict-1-32       	    2352	    495323 ns/op	  24.49 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-210569-level-fastest-dict-1
    BenchmarkEncodeAllDict/length-210569-level-fastest-dict-1-32     	    1052	   1115971 ns/op	 188.69 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-210569-level-default-dict-1
    BenchmarkEncodeAllDict/length-210569-level-default-dict-1-32     	     673	   1763746 ns/op	 119.39 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-210569-level-better-dict-1
    BenchmarkEncodeAllDict/length-210569-level-better-dict-1-32      	     423	   2820325 ns/op	  74.66 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-102605-level-fastest-dict-1
    BenchmarkEncodeAllDict/length-102605-level-fastest-dict-1-32     	    2352	    494047 ns/op	 207.68 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-102605-level-default-dict-1
    BenchmarkEncodeAllDict/length-102605-level-default-dict-1-32     	    1142	   1049912 ns/op	  97.73 MB/s	       0 B/op	       0 allocs/op
    BenchmarkEncodeAllDict/length-102605-level-better-dict-1
    BenchmarkEncodeAllDict/length-102605-level-better-dict-1-32      	     662	   1791540 ns/op	  57.27 MB/s	       0 B/op	       0 allocs/op
    ```

--- zstd/dict_test.go
+++ zstd/dict_test.go
@@ -2,6 +2,7 @@ package zstd
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strings"
 	"testing"
@@ -67,6 +68,245 @@ func TestDecoder_SmallDict(t *testing.T) {
 				t.Fatal(err)
 			}
 		})
+	}
+}
+
+func TestEncoder_SmallDict(t *testing.T) {
+	// All files have CRC
+	fn := "testdata/dict-tests-small.zip"
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	zr, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	var dicts [][]byte
+	var encs []*Encoder
+	var noDictEncs []*Encoder
+	var encNames []string
+
+	for _, tt := range zr.File {
+		if !strings.HasSuffix(tt.Name, ".dict") {
+			continue
+		}
+		func() {
+			r, err := tt.Open()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer r.Close()
+			in, err := ioutil.ReadAll(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+			dicts = append(dicts, in)
+			for level := SpeedFastest; level < speedLast; level++ {
+				enc, err := NewWriter(nil, WithEncoderConcurrency(1), WithEncoderDict(in), WithEncoderLevel(level), WithWindowSize(1<<17))
+				if err != nil {
+					t.Fatal(err)
+				}
+				encs = append(encs, enc)
+				encNames = append(encNames, fmt.Sprint("level-", level.String(), "-dict-", len(dicts)))
+
+				enc, err = NewWriter(nil, WithEncoderConcurrency(1), WithEncoderLevel(level), WithWindowSize(1<<17))
+				if err != nil {
+					t.Fatal(err)
+				}
+				noDictEncs = append(noDictEncs, enc)
+			}
+		}()
+	}
+	dec, err := NewReader(nil, WithDecoderConcurrency(1), WithDecoderDicts(dicts...))
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer dec.Close()
+	for i, tt := range zr.File {
+		if testing.Short() && i > 100 {
+			break
+		}
+		if !strings.HasSuffix(tt.Name, ".zst") {
+			continue
+		}
+		r, err := tt.Open()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer r.Close()
+		in, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		decoded, err := dec.DecodeAll(in, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run("encodeall-"+tt.Name, func(t *testing.T) {
+			// Attempt to compress with all dicts
+			var b []byte
+			var tmp []byte
+			for i := range encs {
+				i := i
+				t.Run(encNames[i], func(t *testing.T) {
+					b = encs[i].EncodeAll(decoded, b[:0])
+					tmp, err = dec.DecodeAll(in, tmp[:0])
+					if err != nil {
+						t.Fatal(err)
+					}
+					if !bytes.Equal(tmp, decoded) {
+						t.Fatal("output mismatch")
+					}
+
+					tmp = noDictEncs[i].EncodeAll(decoded, tmp[:0])
+
+					if strings.Contains(t.Name(), "dictplain") && strings.Contains(t.Name(), "dict-1") {
+						t.Log("reference:", len(in), "no dict:", len(tmp), "with dict:", len(b), "SAVED:", len(tmp)-len(b))
+						// Check that we reduced this significantly
+						if len(b) > 250 {
+							t.Error("output was bigger than expected")
+						}
+					}
+				})
+			}
+		})
+		t.Run("stream-"+tt.Name, func(t *testing.T) {
+			// Attempt to compress with all dicts
+			var tmp []byte
+			for i := range encs {
+				i := i
+				enc := encs[i]
+				t.Run(encNames[i], func(t *testing.T) {
+					var buf bytes.Buffer
+					enc.Reset(&buf)
+					_, err := enc.Write(decoded)
+					if err != nil {
+						t.Fatal(err)
+					}
+					err = enc.Close()
+					if err != nil {
+						t.Fatal(err)
+					}
+					tmp, err = dec.DecodeAll(buf.Bytes(), tmp[:0])
+					if err != nil {
+						t.Fatal(err)
+					}
+					if !bytes.Equal(tmp, decoded) {
+						t.Fatal("output mismatch")
+					}
+					var buf2 bytes.Buffer
+					noDictEncs[i].Reset(&buf2)
+					noDictEncs[i].Write(decoded)
+					noDictEncs[i].Close()
+
+					if strings.Contains(t.Name(), "dictplain") && strings.Contains(t.Name(), "dict-1") {
+						t.Log("reference:", len(in), "no dict:", buf2.Len(), "with dict:", buf.Len(), "SAVED:", buf2.Len()-buf.Len())
+						// Check that we reduced this significantly
+						if buf.Len() > 250 {
+							t.Error("output was bigger than expected")
+						}
+					}
+				})
+			}
+		})
+	}
+}
+
+func BenchmarkEncodeAllDict(b *testing.B) {
+	fn := "testdata/dict-tests-small.zip"
+	data, err := ioutil.ReadFile(fn)
+	t := testing.TB(b)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	zr, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	var dicts [][]byte
+	var encs []*Encoder
+	var noDictEncs []*Encoder
+	var encNames []string
+
+	for _, tt := range zr.File {
+		if !strings.HasSuffix(tt.Name, ".dict") {
+			continue
+		}
+		func() {
+			r, err := tt.Open()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer r.Close()
+			in, err := ioutil.ReadAll(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+			dicts = append(dicts, in)
+			for level := SpeedFastest; level < speedLast; level++ {
+				enc, err := NewWriter(nil, WithEncoderConcurrency(1), WithEncoderDict(in), WithEncoderLevel(level), WithWindowSize(1<<17))
+				if err != nil {
+					t.Fatal(err)
+				}
+				encs = append(encs, enc)
+				encNames = append(encNames, fmt.Sprint("level-", level.String(), "-dict-", len(dicts)))
+
+				enc, err = NewWriter(nil, WithEncoderConcurrency(1), WithEncoderLevel(level), WithWindowSize(1<<17))
+				if err != nil {
+					t.Fatal(err)
+				}
+				noDictEncs = append(noDictEncs, enc)
+			}
+		}()
+	}
+	dec, err := NewReader(nil, WithDecoderConcurrency(1), WithDecoderDicts(dicts...))
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer dec.Close()
+
+	for i, tt := range zr.File {
+		if i == 5 {
+			break
+		}
+		if !strings.HasSuffix(tt.Name, ".zst") {
+			continue
+		}
+		r, err := tt.Open()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer r.Close()
+		in, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		decoded, err := dec.DecodeAll(in, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i := range encs {
+			// Only do 1 dict (3 encoders) for now.
+			if i == 3 {
+				break
+			}
+			// Attempt to compress with all dicts
+			var dst []byte
+			enc := encs[i]
+			b.Run(fmt.Sprintf("length-%d-%s", len(decoded), encNames[i]), func(b *testing.B) {
+				b.SetBytes(int64(len(decoded)))
+				b.ResetTimer()
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					dst = enc.EncodeAll(decoded, dst[:0])
+				}
+			})
+		}
 	}
 }
 
