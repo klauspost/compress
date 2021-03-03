c00d109633b2b20a59d81c0168e78f57f41f9502
Klaus Post <klauspost@gmail.com>
2021-03-03 11:29:54 +0100

    Add command.

--- s2/cmd/_s2sx/gensfx.sh
+++ s2/cmd/_s2sx/gensfx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-GOOS=linux GOARCH=amd64 build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
-GOOS=darwin GOARCH=amd64 build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
-GOOS=windows GOARCH=amd64 build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
+GOOS=linux GOARCH=amd64 go build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
+GOOS=darwin GOARCH=amd64 go build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
+GOOS=windows GOARCH=amd64 go build -o ./sfx-exe/$GOOS-$GOARCH ./_unpack/main.go
