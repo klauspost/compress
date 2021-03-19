b0e4cf6303706e8bf39dcbf16ce8d6fb062eb518
Jacalz <jacob.alzen@gmail.com>
2021-03-19 14:57:10 +0100

    Add in changes for s2 package
    There is a few errors left as reported, which might be worth taking a look at:
    
    compress/s2/encode.go
      (402, 19)  SA6002  argument should be pointer-like to avoid allocations
      (655, 18)  SA6002  argument should be pointer-like to avoid allocations
      (734, 17)  SA6002  argument should be pointer-like to avoid allocations
      (819, 17)  SA6002  argument should be pointer-like to avoid allocations

--- s2/decode.go
+++ s2/decode.go
@@ -43,10 +43,6 @@ func decodedLen(src []byte) (blockLen, headerLen int, err error) {
 	return int(v), n, nil
 }
 
-const (
-	decodeErrCodeCorrupt = 1
-)
-
 // Decode returns the decoded form of src. The returned slice may be a sub-
 // slice of dst if dst was large enough to hold the entire decoded block.
 // Otherwise, a newly allocated slice will be returned.
