6c883c8efdcce80f36e56b9eea6dd6da097a3928
Klaus Post <klauspost@gmail.com>
2021-03-19 16:31:17 +0100

    s2: Clean up and minor performance improvement in best

--- s2/encode_all.go
+++ s2/encode_all.go
@@ -12,16 +12,11 @@ import (
 )
 
 func load32(b []byte, i int) uint32 {
-	b = b[i:]
-	b = b[:4]
-	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+	return binary.LittleEndian.Uint32(b[i:])
 }
 
 func load64(b []byte, i int) uint64 {
-	b = b[i:]
-	b = b[:8]
-	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
-		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
+	return binary.LittleEndian.Uint64(b[i:])
 }
 
 // hash6 returns the hash of the lowest 6 bytes of u to fit in a hash table with h bits.
