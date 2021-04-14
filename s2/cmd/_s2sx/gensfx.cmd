1076456cfa6b3bcd9d28ac68965b4a3aa2f85a0a
Klaus Post <klauspost@gmail.com>
2021-04-14 11:16:52 +0200

    test: Switch to github actions (#354)

--- s2/cmd/_s2sx/gensfx.cmd
+++ s2/cmd/_s2sx/gensfx.cmd
@@ -4,28 +4,29 @@ DEL /Q sfx-exe\*
 
 SET GOOS=linux
 SET GOARCH=amd64
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+SET BUILDFLAGS=-trimpath -ldflags="-s -w"
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=arm64
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=arm
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=ppc64le
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=mips64
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 
 
 SET GOOS=darwin
 SET GOARCH=amd64
-go build  -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build  %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=arm64
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 
 SET GOOS=windows
 SET GOARCH=amd64
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 SET GOARCH=386
-go build -ldflags="-s -w" -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
+go build %BUILDFLAGS% -o ./sfx-exe/%GOOS%-%GOARCH% ./_unpack/main.go
 
 s2c.exe -rm -slower sfx-exe\*
 DEL /Q s2c.exe
