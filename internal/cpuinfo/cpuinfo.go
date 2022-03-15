9f4fdd100525d62b771d8b71bf852507d6c799e4
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2022-03-15 17:38:45 +0100

    cpuinfo: get x86 features in runtime
    For now we need to know whether an x86 CPU supports the BMI extensions.

---
+++ internal/cpuinfo/cpuinfo.go
@@ -0,0 +1,24 @@
+// Package cpuinfo gives runtime info about the current CPU.
+//
+// This is a very limited module meant for use internally
+// in this project. For more versatile solution check
+// https://github.com/klauspost/cpuid.
+package cpuinfo
+
+// HasBMI1 checks whether an x86 CPU supports the BMI1 extension.
+func HasBMI1() bool {
+	return hasBMI1
+}
+
+// HasBMI2 checks whether an x86 CPU supports the BMI2 extension.
+func HasBMI2() bool {
+	return hasBMI2
+}
+
+// HasBMI checks whether an x86 CPU supports both BMI1 and BMI2 extensions.
+func HasBMI() bool {
+	return HasBMI1() && HasBMI2()
+}
+
+var hasBMI1 bool
+var hasBMI2 bool
