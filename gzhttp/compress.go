a0c3660993d81e041681c4347d861416b044dee4
rtribotte <rtribotte@users.noreply.github.com>
2023-05-02 16:53:30 +0200

    review: typo

--- gzhttp/compress.go
+++ gzhttp/compress.go
@@ -302,7 +302,7 @@ func (w *GzipResponseWriter) startPlain() error {
 }
 
 // WriteHeader just saves the response code until close or GZIP effective writes.
-// In the specific case of 1xx status codes, WriteHeader is directly calling the wrapped ResponseWriter,
+// In the specific case of 1xx status codes, WriteHeader is directly calling the wrapped ResponseWriter.
 func (w *GzipResponseWriter) WriteHeader(code int) {
 	// Handle informational headers
 	if code >= 100 && code <= 199 {
