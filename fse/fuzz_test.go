027807ed96323a242ed36d408255d83f0c22e8f3
Klaus Post <klauspost@gmail.com>
2023-10-07 13:36:51 +0200

    Change type to TypeGoFuzz

--- fse/fuzz_test.go
+++ fse/fuzz_test.go
@@ -32,7 +32,8 @@ func FuzzCompress(f *testing.F) {
 }
 
 func FuzzDecompress(f *testing.F) {
-	fuzz.AddFromZip(f, "testdata/fse_decompress.zip", fuzz.TypeRaw, false)
+	// Input is mixed, but TypeGoFuzz will fall back to raw input.
+	fuzz.AddFromZip(f, "testdata/fse_decompress.zip", fuzz.TypeGoFuzz, false)
 	f.Fuzz(func(t *testing.T, buf0 []byte) {
 		var s2 Scratch
 		s2.DecompressLimit = 128 << 10
