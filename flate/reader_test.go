a4773a95c30044de0082747b2319dfcba3f7b7e1
fakefloordiv <real.floordiv@gmail.com>
2023-10-08 21:13:52 +0200

    made benchmarks more accurate by removing io.Copy call
    as io.Copy is just an alias for io.CopyBuffer, but passing nil instead of the actual buffer, it is being allocated internally. So better to pass it manually

--- flate/reader_test.go
+++ flate/reader_test.go
@@ -64,13 +64,15 @@ func benchmarkDecode(b *testing.B, testfile, level, n int) {
 	w.Close()
 	buf1 := compressed.Bytes()
 	buf0, compressed, w = nil, nil, nil
+	const ioCopyBuffSize = 32 * 1024 // taken from io.copyBuffer, in case passed buf==nil
+	ioCopyBuff := make([]byte, ioCopyBuffSize)
 	runtime.GC()
 	b.StartTimer()
 	r := NewReader(bytes.NewReader(buf1))
 	res := r.(Resetter)
 	for i := 0; i < b.N; i++ {
 		res.Reset(bytes.NewReader(buf1), nil)
-		io.Copy(io.Discard, r)
+		io.CopyBuffer(io.Discard, r, ioCopyBuff)
 	}
 }
 
