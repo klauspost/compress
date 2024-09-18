26519f82c1b4cb74d671da45c513bc4e6151c3f8
Klaus Post <klauspost@gmail.com>
2024-09-18 03:34:52 -0700

    zstd: Improve memory usage on small streaming encodes (#1007)
    * zstd: Improve memory usage on small streaming encodes
    
    Very small streams will use EncodeAll internally when closing and no header has been written.
    
    This will pull a new encoder from the async buffer.
    
    Instead re-use the stream encoder.
    
    Before:
    ```
    BenchmarkMem/flush-32         	    1359	    837989 ns/op	 7376959 B/op	     109 allocs/op
    BenchmarkMem/no-flush-32      	     129	   8884753 ns/op	112044489 B/op	     254 allocs/op
    ```
    
    After:
    ```
    BenchmarkMem/flush-32         	    1254	    922593 ns/op	 7376966 B/op	     109 allocs/op
    BenchmarkMem/no-flush-32      	    1488	    841270 ns/op	 7374164 B/op	      29 allocs/op
    ```
    
    Test is pretty much worst case, but shows the issue nicely.
    
    * Go mod fun

--- zstd/_generate/go.mod
+++ zstd/_generate/go.mod
@@ -1,16 +1,18 @@
 module github.com/klauspost/compress/s2/_generate
 
-go 1.19
+go 1.22.0
+
+toolchain go1.22.4
 
 require (
 	github.com/klauspost/compress v1.15.15
-	github.com/mmcloughlin/avo v0.5.0
+	github.com/mmcloughlin/avo v0.6.0
 )
 
 require (
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/tools v0.25.0 // indirect
 )
 
 replace github.com/klauspost/compress => ../..
--- zstd/zstd_test.go
+++ zstd/zstd_test.go
@@ -6,9 +6,12 @@ package zstd
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"strings"
 	"testing"
 	"time"
 )
@@ -58,4 +61,113 @@ func TestMatchLen(t *testing.T) {
 		check(a[:l], b, l)
 		a[l] = ^a[l]
 	}
+}
+
+func TestWriterMemUsage(t *testing.T) {
+	testMem := func(t *testing.T, fn func()) {
+		var before, after runtime.MemStats
+		var w io.Writer
+		if false {
+			f, err := os.Create(strings.ReplaceAll(fmt.Sprintf("%s.pprof", t.Name()), "/", "_"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+			w = f
+			t.Logf("opened memory profile %s", t.Name())
+		}
+		runtime.GC()
+		runtime.ReadMemStats(&before)
+		fn()
+		runtime.GC()
+		runtime.ReadMemStats(&after)
+		if w != nil {
+			pprof.WriteHeapProfile(w)
+		}
+		t.Log("wrote profile")
+		t.Logf("%s: Memory Used: %dMB, %d allocs", t.Name(), (after.HeapInuse-before.HeapInuse)/1024/1024, after.HeapObjects-before.HeapObjects)
+	}
+	data := make([]byte, 10<<20)
+
+	t.Run("enc-all-lower", func(t *testing.T) {
+		for level := SpeedFastest; level <= SpeedBestCompression; level++ {
+			t.Run(fmt.Sprint("level-", level), func(t *testing.T) {
+				var zr *Encoder
+				var err error
+				dst := make([]byte, 0, len(data)*2)
+				testMem(t, func() {
+					zr, err = NewWriter(io.Discard, WithEncoderConcurrency(32), WithEncoderLevel(level), WithLowerEncoderMem(false), WithWindowSize(1<<20))
+					if err != nil {
+						t.Fatal(err)
+					}
+					for i := 0; i < 100; i++ {
+						_ = zr.EncodeAll(data, dst[:0])
+					}
+				})
+				zr.Close()
+			})
+		}
+	})
+}
+
+var data = []byte{1, 2, 3}
+
+func newZstdWriter() (*Encoder, error) {
+	return NewWriter(
+		io.Discard,
+		WithEncoderLevel(SpeedBetterCompression),
+		WithEncoderConcurrency(16), // we implicitly get this concurrency level if we run on 16 core CPU
+		WithLowerEncoderMem(false),
+		WithWindowSize(1<<20),
+	)
+}
+
+func BenchmarkMem(b *testing.B) {
+	b.Run("flush", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			w, err := newZstdWriter()
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			for j := 0; j < 16; j++ {
+				w.Reset(io.Discard)
+
+				if _, err := w.Write(data); err != nil {
+					b.Fatal(err)
+				}
+
+				if err := w.Flush(); err != nil {
+					b.Fatal(err)
+				}
+
+				if err := w.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+		}
+	})
+	b.Run("no-flush", func(b *testing.B) {
+		// Will use encodeAll for block.
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			w, err := newZstdWriter()
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			for j := 0; j < 16; j++ {
+				w.Reset(io.Discard)
+
+				if _, err := w.Write(data); err != nil {
+					b.Fatal(err)
+				}
+
+				if err := w.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+		}
+	})
 }
